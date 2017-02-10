63bbe179d4d147b0f8d06c4aef5324335334913f
Audrey Serene <audrey@Audreys-MBP.home>
2017-02-10 16:56:16 -0600

    finish desktop

--- pokemon/templatetags/get_posts.py
+++ pokemon/templatetags/get_posts.py
@@ -4,10 +4,18 @@ from forum.models import Post, Category
 
 register = Library()
 
-@register.simple_tag
+@register.assignment_tag
 def get_posts():
 	post_category = Category.objects.get(name="Announcements")
 	get_posts = Post.objects.filter(category=post_category).order_by("-post_time")[:6]
 	for post in get_posts:
 		post.body_length = 64 - len(post.title)
 	return get_posts
+
+@register.assignment_tag
+def get_3_posts():
+	post_category = Category.objects.get(name="Announcements")
+	get_3_posts = Post.objects.filter(category=post_category).order_by("-post_time")[:3]
+	for post in get_3_posts:
+		post.body_length = 72 - len(post.title)
+	return get_3_posts
